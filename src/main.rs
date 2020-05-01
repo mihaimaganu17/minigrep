1667b038a322d8365272386d6dc3a675fbe5477d
mihaimaganu17 <mihai_gx_maganu@yahoo.com>
2020-05-01 22:01:56 +0300

    Finished minigrpe from Rust book

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+/* Used to parse cmdline args */
+use std::env;
+use std::process;
+
+use minigrep::*;
+
 fn main() {
-    println!("Hello, world!");
+  /* Call collect to turn the args Iterator into a collection */
+  let args: Vec<String> = env::args().collect();
+
+  let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
+    eprintln! ("Problem parsing arguments: {}", err);
+    process::exit(1);
+  });
+
+  if let Err(e) = minigrep::run(config) {
+    eprintln! ("Application error: {}", e);
+    process::exit(1);
+  }
 }
+
